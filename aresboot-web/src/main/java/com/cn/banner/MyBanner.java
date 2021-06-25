087a64eb6d6b468aca7900b6f1a91bdde84b6b52
532960400@qq.com <xy404044>
2021-06-25 15:06:50 +0800

    aresboot首次提交

---
+++ aresboot-web/src/main/java/com/cn/banner/MyBanner.java
@@ -0,0 +1,13 @@
+package com.cn.banner;
+
+import org.springframework.boot.Banner;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
+
+public class MyBanner implements Banner {
+    @Override
+    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+
+    }
+}
