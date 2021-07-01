397821ba9cb6dbe4aac702bc842a4aadbb56d774
532960400@qq.com <xy404044>
2021-07-01 09:04:24 +0800

    bt20210603

--- aresboot-web/src/main/java/com/cn/vo/Message.java
+++ aresboot-web/src/main/java/com/cn/vo/Message.java
@@ -1,14 +1,19 @@
 package com.cn.vo;
 
 
-import lombok.Builder;
 import lombok.Data;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
 @Data // 这就是lombok注解，这个注解使用的是最频繁的
-@Builder // 建造者模式
+@XmlRootElement // 培配置了xml根元素
 public class Message {
+    @XmlElement
     private String title;
+    @XmlElement
     private Date pubdate;
+    @XmlElement
     private String content;
 }
