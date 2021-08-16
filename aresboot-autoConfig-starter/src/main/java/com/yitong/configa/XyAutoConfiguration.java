ab0c26a04e779ea9229f36ac3dbb39c1db6b3545
532960400@qq.com <xy404044>
2021-08-16 11:25:25 +0800

    xx

--- aresboot-autoConfig-starter/src/main/java/com/yitong/configa/XyAutoConfiguration.java
+++ aresboot-autoConfig-starter/src/main/java/com/yitong/configa/XyAutoConfiguration.java
@@ -9,14 +9,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author xy
  * @date 2021-8-9
  */
 @Configuration
-//@EnableConfigurationProperties(Detp.class)
+@EnableConfigurationProperties(Detp.class)
+//@Import(Detp.class)
 //@Import(DefaultSelectorImport.class)
-@Import(DefaultRegistBean.class)
+//@Import(DefaultRegistBean.class)
 public class XyAutoConfiguration {
 
+    @Bean(name = "books")
+    public List<String> getBookList(){
+        List<String> list = new ArrayList<>();
+        list.add("boot1");
+        list.add("boot2");
+        list.add("boot3");
+        list.add("boot4");
+        list.add("boot5");
+        return list;
+    }
 }
