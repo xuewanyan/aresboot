c19268310835feed3e6b1b968ff6d9460c250421
532960400@qq.com <xy404044>
2021-08-16 15:36:32 +0800

    reset

--- aresboot-web/src/test/java/MessageTest.java
+++ aresboot-web/src/test/java/MessageTest.java
@@ -35,6 +35,7 @@ public class MessageTest {
 
     @Test
     public Class<?> messageService() throws ClassNotFoundException {
+        // 测试reset
         StackTraceElement[] stackTrace = new RuntimeException().getStackTrace();
         for (StackTraceElement stackTraceElement : stackTrace) {
             if ("main".equals(stackTraceElement.getMethodName())) {
